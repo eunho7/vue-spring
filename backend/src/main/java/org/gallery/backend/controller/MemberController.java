9e7bd733e4d4fc99822d1a414ec8ac9b883e1829
Eunho <76768140sa@gmail.com>
2024-10-21 00:07:36 +0900

    feat: jwt 쿠키값 전달 #5

--- backend/src/main/java/org/gallery/backend/controller/MemberController.java
+++ backend/src/main/java/org/gallery/backend/controller/MemberController.java
@@ -1,8 +1,13 @@
 package org.gallery.backend.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.gallery.backend.dto.MemberDTO;
+import org.gallery.backend.jwt.JwtService;
+import org.gallery.backend.jwt.JwtServiceImpl;
 import org.gallery.backend.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +23,22 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/api/account/login")
-    public ResponseEntity<Long> login(@RequestBody Map<String, String> params) {
+    public ResponseEntity login(@RequestBody Map<String, String> params,
+                                        HttpServletResponse response) {
         MemberDTO memberDTO = memberService.findByEmailAndPassword(params.get("email"), params.get("password"));
         if(memberDTO!=null) {
-            return ResponseEntity.ok()
-                    .body(memberDTO.getId());
+            JwtService jwtService = new JwtServiceImpl();
+            Long id = memberDTO.getId();
+            String token = jwtService.getToken("id", id);
+
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true); // 자바스크립트 접근 금지
+            cookie.setPath("/");
+
+            response.addCookie(cookie);
+
+            return ResponseEntity.ok().
+                    body(memberDTO.getId());
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
