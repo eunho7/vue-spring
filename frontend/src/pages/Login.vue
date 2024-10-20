c07d2f0e51f8c98ef57ed4190dcfe66e02349e8f
Eunho <76768140sa@gmail.com>
2024-10-20 22:13:14 +0900

    feat: 로그아웃 프론트 및 세션 #3

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -23,6 +23,7 @@
 </template>
 
 <script>
+import router from '@/scripts/router';
 import store from '@/scripts/store';
 import axios from 'axios';
 import { reactive } from 'vue';
@@ -39,7 +40,11 @@ export default {
         const submit = () => {
             axios.post("/api/account/login", state.form).then((response)=>{
               store.commit("setAccount", response.data);
-                window.alert("로그인하였습니다.");
+              sessionStorage.setItem("id", response.data);
+              router.push({path:"/"});
+              window.alert("로그인하였습니다.");
+            }).catch(()=>{
+              window.alert("로그인 정보가 존재하지 않습니다.");
             })
         }
 
